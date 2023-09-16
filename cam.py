0cc3bf2cfbc8f5a9b96dcf5796ae0ac2d51a6e42
Shivam <91791239+mavihsrr@users.noreply.github.com>
2023-09-17 03:18:50 +0530

    Add files via upload

---
+++ cam.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+from imutils.video import VideoStream
+
+
+
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret,frame = cam.read()
+
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) == ord('x'):
+        print("Exiting...")
+        break
+
+    elif cv2.waitKey(1) == ord('s'): 
+            cv2.imwrite(filename='test.jpg', img=frame)
+            cam.release()
+            img_new = cv2.imread('test.jpg')
+            img_new = cv2.imshow("Captured Image", img_new)
+            cv2.waitKey(1650)
+            cv2.destroyAllWindows()
+            print("saved image...")
+
+        
+            break
+
+
+cam.release()
+cv2.destroyAllWindows()
+
